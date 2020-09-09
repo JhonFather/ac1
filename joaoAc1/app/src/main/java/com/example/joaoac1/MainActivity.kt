b213dc6d98b193677dee7e2e7d5a626de68c137d
joao.vinicius <joao.vinicius@linx.com.br>
2020-09-09 18:15:15 -0300

    continuada 1

---
+++ joaoAc1/app/src/main/java/com/example/joaoac1/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.joaoac1
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+        fun calculaIdade(componente: View) {
+            val nome = campo2.text.toString()
+            val pai = campo3.text.toString().toInt()
+            val mae = campo4.text.toString().toInt()
+            if(nome == "" || nome.length <=3 || pai <= 0 || mae <= 0){
+                Toast.makeText(this, "Os campos não podem ser nulos ou zerados", Toast.LENGTH_SHORT).show()
+            }else {
+                val idadeMorte = (pai + mae)-30
+
+                if(idadeMorte > 60){
+                    campo6.setTextColor(Color.GREEN)
+                    campo6.text = "${nome} você vai morrer com ${idadeMorte} - velho"
+                }else if(idadeMorte > 20){
+                    campo6.setTextColor(Color.YELLOW)
+                    campo6.text = "${nome} você vai morrer com ${idadeMorte} - jovem"
+                }else {
+                    campo6.setTextColor(Color.RED)
+                    campo6.text = "${nome} você vai morrer com ${idadeMorte} - MUITO jovem"
+                }
+
+            }
+        }
+}
