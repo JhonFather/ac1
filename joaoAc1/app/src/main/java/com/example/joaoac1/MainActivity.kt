bf8a0d89d027626cff0332a55e90cd93f8e3fb89
joao.vinicius <joao.vinicius@linx.com.br>
2020-09-09 18:30:33 -0300

    continuada 1

--- joaoAc1/app/src/main/java/com/example/joaoac1/MainActivity.kt
+++ joaoAc1/app/src/main/java/com/example/joaoac1/MainActivity.kt
@@ -24,13 +24,13 @@ class MainActivity : AppCompatActivity() {
 
                 if(idadeMorte > 60){
                     campo6.setTextColor(Color.GREEN)
-                    campo6.text = "${nome} você vai morrer com ${idadeMorte} - velho"
+                    campo6.text = "${nome} você vai morrer com ${idadeMorte} anos. Vai morrer velho"
                 }else if(idadeMorte > 20){
                     campo6.setTextColor(Color.YELLOW)
-                    campo6.text = "${nome} você vai morrer com ${idadeMorte} - jovem"
+                    campo6.text = "${nome} você vai morrer com ${idadeMorte} anos. Vai morrer jovem"
                 }else {
                     campo6.setTextColor(Color.RED)
-                    campo6.text = "${nome} você vai morrer com ${idadeMorte} - MUITO jovem"
+                    campo6.text = "${nome} você vai morrer com ${idadeMorte} anos. Vai morrer MUITO jovem"
                 }
 
             }
